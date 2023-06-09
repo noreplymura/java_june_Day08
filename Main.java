42d1d0f2e6a83f544b63585e63f993f0759b9e65
noreplymura <119089270+noreplymura@users.noreply.github.com>
2023-06-09 09:17:24 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        int[] age = { 12, 4, 5, 2, 5 };
+        System.out.println("Accessing Elements of Array");
+        System.out.println("First Element"+age[0]);
+        System.out.println("second Element "+age[2]);
+        System.out.println("Third Element"+age[3]);
+        System.out.println("four Element "+age[4]);
+        System.out.println("fifith Element" + age[5]);
+    }
+}
